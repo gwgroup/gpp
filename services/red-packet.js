84aec7bb78967f7259b453b64e8fda8ab7d21c8d
qunpeng.li <newbreach@live.com>
2019-01-25 18:11:07 +0800

    user incr

--- services/red-packet.js
+++ services/red-packet.js
@@ -3,10 +3,11 @@ var util = require('../utils/index'),
 const BATCH_NO_KEY = 'rh_red_packet_batch_no';
 /**
  * 生成批次单号
+ * @param {String} user_id 用户ID
  * @param {Function} cb 
  */
-function generateBatchNo(cb) {
-    redisClient.incr(BATCH_NO_KEY, (err, result) => {
+function generateBatchNo(user_id,cb) {
+    redisClient.incr(`${BATCH_NO_KEY}_${user_id}`, (err, result) => {
         if (err) {
             return cb(err);
         }
