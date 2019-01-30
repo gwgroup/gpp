1fc27b70c27c01570f0dab0a1d6c8bb4a5597bc0
qunpeng.li <newbreach@live.com>
2019-01-30 17:15:48 +0800

    红包活动,卡路由

--- routes/api/red-packet.js
+++ routes/api/red-packet.js
@@ -1,4 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var redPacketService = require('../../services/red-packet');
 
-module.exports=router;
+/**
+ * 创建红包发放计划
+ */
+router.post('/create_active', function (req, res, next) {
+    redPacketService.createRedPacketActive(req.body, req.token.user_id, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(JSON.stringify({ code: 1000 }));
+    });
+});
+
+/**
+ * 生成红包卡
+ */
+router.post('/generate_cards', function (req, res, next) {
+    redPacketService.generateRedPacketCards(req.body, req.token.user_id, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(JSON.stringify({ code: 1000 }));
+    });
+});
+module.exports = router;
